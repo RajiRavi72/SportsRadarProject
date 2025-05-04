ebe2037c3f6a835ac2bc27ae79b911232b73c16c
RajiRavi72 <rajalakshmiravishankar1@gmail.com>
2025-05-04 16:25:08 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,12 +4,7 @@ def db_conn():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Raji",   
-        database="sportsradar_db"
+        password="Raji",  # give your password here 
+        database="sportsradar_db"  # give your db name here
     )
 
-# import pymysql
-# from sqlalchemy import create_engine
-# def db_conn():
-#     conn = create_engine("mysql+pymysql://root:Raji@localhost/sportsradar_db")  
-#     return conn
